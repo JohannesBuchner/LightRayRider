26f311b7729a910c588e0749e485ac8fb7d4aedf
Johannes Buchner <johannes.buchner.acad@gmx.com>
2026-09-05 23:13:59 +0200

    use install, because cython module may not be importable

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+#sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('_ext'))
 
 import sphinx_rtd_theme
